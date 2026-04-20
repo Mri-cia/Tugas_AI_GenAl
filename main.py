44b6d0d4464bd684c4074abc7cedbf331e09b6f5
Mri-cia <rawikara8c@gmail.com>
2026-04-20 16:05:05 +0700

    add fitness function

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import math
 import util 
-import decode as ga
+import decode as dc
+import fitness as ft
 
 # Pintu Utama
 def main():
-    ga.decode_cromosome(util.to_bin(1))
-    
+
     print()
 
 if __name__=="__main__":
