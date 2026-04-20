33d9be23f99177b1108fb585fe1a4c576992a485
Mri-cia <rawikara8c@gmail.com>
2026-04-21 06:44:50 +0700

    fix fitness formula

--- fitness.py
+++ fitness.py
@@ -2,9 +2,9 @@ import math
 
 def fitness_func(x1, x2):
     
-    first_part = - (math.sin(x1) * math.cos(x2) * math.tan(x1 + x2))
+    first_part = (math.sin(x1) * math.cos(x2) * math.tan(x1 + x2))
     second_part = math.pow((1/2), 1 - math.sqrt(math.pow(x2, 2)))
 
-    result = first_part + second_part
+    result = - (first_part + second_part)
 
     return result
